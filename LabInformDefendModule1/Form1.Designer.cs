9418d38a83d43b043d4b9bb33404d216b7943cb0
User <User@USER->
2019-04-10 21:10:01 +0400

    Добавьте файлы проекта.

---
+++ LabInformDefendModule1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace LabInformDefendModule1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.richTextBox2);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 144);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Location = new System.Drawing.Point(417, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(363, 96);
+            this.richTextBox2.TabIndex = 6;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(363, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(414, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Нормализированный текст";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входной текст";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 175);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа. Модуль 1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ LabInformDefendModule1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabInformDefendModule1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Нормализация текста
+        /// </summary>
+        /// <param name="text">Входной текст для обработки</param>
+        /// <returns>Нормализированный текст (лишь с русскими символами и цифрами)</returns>
+        public string Normalize(string text)
+        {
+            text = text.ToLower();
+            text = new string((from c_text in text where char.IsWhiteSpace(c_text) || char.IsLetterOrDigit(c_text) select c_text).ToArray());          
+            text = ReplaceCharEnglishToRussian(text);
+            text = Regex.Replace(text, @"[a-zA-Z]", "");
+            return text;
+        }
+
+        /// <summary>
+        /// Замена английских символов на русских
+        /// </summary>
+        /// <param name="text">Входной текст для замены символов</param>
+        /// <returns>Текст с замененными символами</returns>
+        private string ReplaceCharEnglishToRussian(string text)
+        {
+            Dictionary<char, char> replaces = Replacers();
+            StringBuilder string_b = new StringBuilder(text);
+            foreach (var replaсe in replaces)
+            {
+                string_b.Replace(replaсe.Key, replaсe.Value);
+            }
+            return string_b.ToString();
+        }
+
+        /// <summary>
+        /// Словарь символов заменяемых на русские аналоги
+        /// </summary>
+        /// <returns>Заполненный словарь заменяемых символов</returns>
+        private Dictionary<char, char> Replacers()
+        {
+            Dictionary<char, char> replaces = new Dictionary<char, char>();
+            replaces.Add('a', 'а');
+            replaces.Add('b', 'б');
+            replaces.Add('c', 'с');
+            replaces.Add('e', 'е');
+            replaces.Add('o', 'о');
+            replaces.Add('u', 'и');
+            replaces.Add('r', 'г');
+            replaces.Add('x', 'х');
+            replaces.Add('y', 'у');
+            return replaces;
+        }
+
+        /// <summary>
+        /// Событие при нажатии на элемент меню "Открыть" открывающий входной текст программы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text=Open(openFileDialog1);
+        }
+
+        /// <summary>
+        /// Событие при нажатии на кнопку "->" вызывающий нормализацию входного текста в выходную
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = Normalize(richTextBox1.Text);
+        }
+
+        /// <summary>
+        /// Событие при нажатии на элемент меню "Сохранить" сохраняющий выходной текст программы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save(saveFileDialog1, richTextBox2.Text);
+        }
+
+        /// <summary>
+        /// Сохранение текста в .txt в кодировке UTF8
+        /// </summary>
+        /// <param name="saveFile">Элемент управления сохранением файла</param>
+        /// <param name="text">сохраняемый текст</param>
+        public void Save(SaveFileDialog saveFile, string text)
+        {
+            saveFile.FileName = "OutputText.txt";
+            saveFile.Filter = "Text File | *.txt";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter s_writer = new StreamWriter(saveFile.OpenFile(), Encoding.UTF8);
+                s_writer.WriteLine(text);
+                s_writer.Dispose();
+                s_writer.Close();
+            }
+        }
+
+        /// <summary>
+        /// Открытие текстового файла для вывода текста
+        /// </summary>
+        /// <param name="openFile">Элемент управления открытием файла</param>
+        /// <returns>Выходной текст из файла</returns>
+        public string Open(OpenFileDialog openFile)
+        {
+            string output_text = "";
+            openFile.FileName = "InputText.txt";
+            openFile.Title = "Открыть текстовой файл";
+            openFile.Filter = "TXT files|*.txt";
+            openFile.InitialDirectory = @"C:\";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFile.FileName;
+                string[] lines = File.ReadAllLines(filename);
+
+                for (int a = 0; a < lines.Length; a++)
+                {
+                    output_text += lines[a] + Environment.NewLine;                   
+                }
+            }
+            return output_text;
+        }
+    }
+}
